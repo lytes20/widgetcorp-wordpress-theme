b47cd79e3145bd4eac732e21af3c575b192c201e
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-03-14 19:54:57 +0300

    edited header and footer

--- header.php
+++ header.php
@@ -22,29 +22,21 @@
         });
         </script>
 
-	</head>
+	</head> 
 	<body <?php body_class(); ?>>
 
 		<!-- wrapper -->
-		<div class="wrapper">
+		<div id="page_wrap" class="group">
 
 			<!-- header -->
-			<header class="header clear" role="banner">
+			<header>
 
 					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
+					<div id="logo"></div>
 					<!-- /logo -->
+					<h1>Widget Corp</h1>
+					<p>Est 1987</p>	
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
 
 			</header>
 			<!-- /header -->
